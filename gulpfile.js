c7919f9705d9a87e9e1b18106571dad07b13c99f
feudal <calmis87@gmail.com>
2021-08-01 17:56:17 +0300

    app to docs and gulpfile changed

--- gulpfile.js
+++ gulpfile.js
@@ -11,25 +11,25 @@ const fileinclude = require('gulp-file-include');
 const sourcemaps = require('gulp-sourcemaps');
 
 function includetohtml() {
-  return src('docs/html/*.html')
+  return src('app/html/*.html')
     .pipe(fileinclude({
       prefix: '@@',
       basepath: '@file'
     }))
-    .pipe(dest('docs/'))
+    .pipe(dest('app/'))
     .pipe(browserSync.stream());
 }
 
 function browsersync() {
   browserSync.init({
     server: {
-      baseDir: 'docs/'
+      baseDir: 'app/'
     }
   });
 }
 
 function images() {
-  return src('docs/img/**/*')
+  return src('app/img/**/*')
     .pipe(imagemin([
       imagemin.gifsicle({ interlaced: true }),
       imagemin.mozjpeg({ quality: 75, progressive: true }),
@@ -49,9 +49,9 @@ function cleanDist() {
 }
 
 // 'node_modules/jquery/dist/jquery.js',
-// 'node_modules/@fancydocss/fancybox/dist/jquery.fancybox.js',
+// 'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
 // 'node_modules/mixitup/dist/mixitup.js',
-// 'docs/js/main.js'
+// 'app/js/main.js'
 // 'node_modules/slick-carousel/slick/slick.js',
 // 'node_modules/pikaday/pikaday.js',
 
@@ -59,18 +59,18 @@ function scripts() {
   return src([
       'node_modules/jquery/dist/jquery.js',
       'node_modules/slick-carousel/slick/slick.js',
-      'docs/js/main.js'
+      'app/js/main.js'
     ])
     .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(concat('main.min.js'))
     .pipe(uglify())
     .pipe(sourcemaps.write())
-    .pipe(dest('docs/js'))
+    .pipe(dest('app/js'))
     .pipe(browserSync.stream())
 }
 
 function styles() {
-  return src('docs/scss/style.scss')
+  return src('app/scss/style.scss')
     .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(scss({ outputStyle: 'compressed' }))
     .pipe(autoprefixer({
@@ -79,25 +79,25 @@ function styles() {
     }))
     .pipe(concat('style.min.css'))
     .pipe(sourcemaps.write())
-    .pipe(dest('docs/css'))
+    .pipe(dest('app/css'))
     .pipe(browserSync.stream())
 }
 
 function build() {
   return src([
-      'docs/css/*.css',
-      'docs/fonts/**/*',
-      'docs/js/*.js',
-      'docs/*.html'
-    ], { base: 'docs' })
+      'app/css/*.css',
+      'app/fonts/**/*',
+      'app/js/*.js',
+      'app/*.html'
+    ], { base: 'app' })
     .pipe(dest('docs'))
 }
 
 function watching() {
-  watch(['docs/scss/**/*.scss'], styles);
-  watch(['docs/js/**/*.js', '!docs/js/main.min.js'], scripts);
-  watch(['docs/html/**/*.html']).on('change', browserSync.reload);
-  watch(['docs/html/**/*.html'], includetohtml);
+  watch(['app/scss/**/*.scss'], styles);
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+  watch(['app/html/**/*.html']).on('change', browserSync.reload);
+  watch(['app/html/**/*.html'], includetohtml);
 }
 
 exports.images = images;
